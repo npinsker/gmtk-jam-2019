6d8f9c893682dec2b088978c701dda843cdaaf67
Nathan Pinsker <npinsker@gmail.com>
2019-08-02 22:36:29 -0700

    refactor arcade cabinet common logic

---
+++ source/ArcadeCabinet.hx
@@ -0,0 +1,56 @@
+package;
+import flixel.FlxSprite;
+import flixel.math.FlxRect;
+import nova.input.Focusable;
+import nova.render.FlxLocalSprite;
+import nova.render.TiledBitmapData;
+import nova.utils.BitmapDataUtils;
+import nova.utils.Pair;
+
+class ArcadeCabinet extends FlxLocalSprite implements Focusable {
+	public var mainLayer:FlxLocalSprite;
+	public var foregroundLayer:FlxLocalSprite;
+	
+	public var tiles:TiledBitmapData;
+	public var clipSprites:Array<LocalSpriteWrapper>;
+
+	public function new(cabinetImage:String = null, cabinetOffset:Pair<Int> = null) {
+		super();
+		
+		mainLayer = new FlxLocalSprite();
+		foregroundLayer = new FlxLocalSprite();
+		add(mainLayer);
+		add(foregroundLayer);
+		
+		this.width = 320;
+		this.height = 320;
+		this.clipSprites = [];
+		
+		tiles = new TiledBitmapData('assets/images/arcade_tiles_16x16.png', 16, 16, function(b) {
+			return BitmapDataUtils.scaleFn(4, 4)(b);
+		});
+		
+		if (cabinetImage != null) {
+			var cabinetShell = LocalWrapper.fromGraphic('assets/images/rhythm_cabinet_shell.png', {
+				'scale': [4, 4],
+			});
+			foregroundLayer.add(cabinetShell);
+			cabinetShell.xy = -4 * cabinetOffset;
+		}
+	}
+
+	override public function update(elapsed:Float):Void {
+		for (sprite in clipSprites) {
+			var left:Float = Math.max(0, -sprite.x);
+			var right:Float = sprite.width - Math.max(0, sprite.x - (320 - sprite.width));
+			var up:Float = Math.max(0, -sprite.y);
+			var down:Float = sprite.height - Math.max(0, sprite.y - (320 - sprite.height));
+			sprite._sprite.clipRect = new FlxRect(left, up, right - left, down - up);
+		}
+		super.update(elapsed);
+	}
+
+	public function handleInput():Void {
+		trace("Warning: ArcadeCabinet class does not handle input!");
+	}
+}
--- source/CounterGame.hx
+++ source/CounterGame.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.math.FlxRect;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import nova.input.Focusable;
@@ -11,10 +12,8 @@ import nova.utils.BitmapDataUtils;
 
 using nova.animation.Director;
 
-class CounterGame extends FlxLocalSprite implements Focusable {
+class CounterGame extends ArcadeCabinet {
 	public var reticle:FlxLocalSprite;
-	
-	public var tiles:TiledBitmapData;
 	
 	public var noteSprites:Array<FlxLocalSprite>;
 	
@@ -26,35 +25,14 @@ class CounterGame extends FlxLocalSprite implements Focusable {
 	
 	public var sprites:Array<LocalSpriteWrapper>;
 	
-	public var mainLayer:FlxLocalSprite;
-	public var foregroundLayer:FlxLocalSprite;
-	
 	public var background:FlxLocalSprite;
 	public function new() {
-		super();
+		super('assets/images/rhythm_cabinet_shell.png', [10, 20]);
 		
-		mainLayer = new FlxLocalSprite();
-		foregroundLayer = new FlxLocalSprite();
-		add(mainLayer);
-		add(foregroundLayer);
-		
-		this.width = 320;
-		this.height = 320;
-		
-		tiles = new TiledBitmapData('assets/images/arcade_tiles_16x16.png', 16, 16, function(b) {
-			return BitmapDataUtils.scaleFn(4, 4)(b);
-		});
-		
-		background = LocalWrapper.fromGraphic('assets/images/rhythm_splash.png', {
+		background = LocalWrapper.fromGraphic('assets/images/counter_splash.png', {
 			'scale': [4, 4],
 		});
 		mainLayer.add(background);
-		
-		var cabinetShell = LocalWrapper.fromGraphic('assets/images/rhythm_cabinet_shell.png', {
-			'scale': [4, 4],
-		});
-		foregroundLayer.add(cabinetShell);
-		cabinetShell.xy = [ -40, -80];
 		
 		noteSprites = [];
 		sprites = [];
@@ -76,12 +54,12 @@ class CounterGame extends FlxLocalSprite implements Focusable {
 			animation: [0, 1],
 			frameRate: 3,
 		});
-		sprites.push(potato);
+		clipSprites.push(potato);
 		mainLayer.add(potato);
 		potato.x = -30 - potato.width;
 		potato.y = Math.random() * 380 + 50 - potato.height / 2;
-		Director.moveTo(potato, [320 + 30, Std.int(Math.random() * 380 + 50 - potato.height / 2)], Std.int(60 + Math.random() * 25)).call(function() {
-			sprites.remove(potato);
+		Director.moveTo(potato, [320 + 30, Std.int(Math.random() * 380 + 50 - potato.height / 2)], Std.int(360 + Math.random() * 25)).call(function() {
+			clipSprites.remove(potato);
 			mainLayer.remove(potato);
 		});
 		
@@ -101,7 +79,7 @@ class CounterGame extends FlxLocalSprite implements Focusable {
 	public function handleTap():Void {
 	}
 
-	public function handleInput():Void {
+	override public function handleInput():Void {
 		if (InputController.justPressed(CONFIRM)) {
 			if (!playing) {
 				startGame();
@@ -113,13 +91,5 @@ class CounterGame extends FlxLocalSprite implements Focusable {
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
-		
-		for (sprite in sprites) {
-			var left:Float = Math.max(0, -sprite.x);
-			var right:Float = Math.min(320 - sprite.width, sprite.x);
-			var up:Float = Math.max(0, -sprite.y);
-			var down:Float = Math.min(320 - sprite.height, sprite.y);
-			sprite._sprite.clipRect.set(left, up, right - left, down - up);
-		}
 	}
 }
